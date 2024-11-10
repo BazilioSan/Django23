380dd22a04e710857e8c648623c5bd8991154463
Alexandr <blix@list.ru>
2024-11-10 10:32:39 +0300

    CRUD реализован, все работает

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,16 @@
-from config.urls import path
+from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, ProductListView, ProductDetailView, ProductCreateView
+from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ContactsView, ProductDeleteView, ProductUpdateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="product_list"),
-    path("contacts/", contacts, name="contacts"),
+    path("contacts/", ContactsView.as_view(), name="contacts"),
     path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
-    path("catalog/create", ProductCreateView.as_view(), name="product_create"),
+    path("products/create/", ProductCreateView.as_view(), name="product_create"),
+    path("products/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
+    path("products/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.urls import reverse_lazy
 from django.http import HttpResponse
 from .models import Product
@@ -7,18 +8,28 @@ from .models import Product
 class ProductListView(ListView):
     model = Product
 
-def contacts(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
-        return HttpResponse(f"Спасибо, {name}, за Ваше сообщение! Наши специалисты свяжутся с Вами по указанному номеру телефона!")
-    return render(request, "contacts.html")
+class ContactsView(TemplateView):
+    template_name = "catalog/contacts.html"
 
 class ProductDetailView(DetailView):
     model = Product
 
 class ProductCreateView(CreateView):
+    def get_object(self, queryset=None):
+        # Update view counter
+        self.object = super().get_object(queryset)
+        self.object.view_counter += 1
+        self.object.save()
+        return self.object
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+class ProductUpdateView(UpdateView):
     model = Product
     fields = ("title", "description", "price", "image", "category")
     success_url = reverse_lazy('catalog:product_list')
+    def get_success_url(self):
+        return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
