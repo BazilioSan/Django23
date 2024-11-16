05d35d0a7ecf49d195fbecd3758c3acdf8f65a60
Alexandr <blix@list.ru>
2024-11-16 11:45:58 +0300

    Формы для CrUd + init 26.1

--- catalog/views.py
+++ catalog/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView, DetailView, TemplateView
 from django.urls import reverse_lazy
 from django.http import HttpResponse
 from .models import Product
+from .forms import ProductForm
 
 class ProductListView(ListView):
     model = Product
@@ -15,11 +16,18 @@ class ProductDetailView(DetailView):
     model = Product
 
 class ProductCreateView(CreateView):
-    def get_object(self, queryset=None):
-        self.object = super().get_object(queryset)
-        self.object.view_counter += 1
-        self.object.save()
-        return self.object
+    model = Product
+    # fields = ('title', 'description', 'price', 'image', 'category')
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+    # def get_object(self, queryset=None):
+    #     self.object = super().get_object(queryset)
+    #     self.object.view_counter += 1
+    #     self.object.save()
+    #     return self.object
+
 
 class ProductDeleteView(DeleteView):
     model = Product
@@ -28,7 +36,11 @@ class ProductDeleteView(DeleteView):
 
 class ProductUpdateView(UpdateView):
     model = Product
-    fields = ("title", "description", "price", "image", "category")
+    form_class = ProductForm
+    # fields = ("title", "description", "price", "image", "category")
+    template_name = "catalog/product_form.html"
     success_url = reverse_lazy('catalog:product_list')
+
     def get_success_url(self):
         return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
+
