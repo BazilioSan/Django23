7e15ccaae28487009271950ce56561eb29da4243
Alexandr <blix@list.ru>
2024-11-20 00:47:44 +0300

    Админ ок, формы в работе

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.views.generic import ListView, DetailView, TemplateView
 from django.urls import reverse_lazy
-from django.http import HttpResponse
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Product
 from .forms import ProductForm
 
@@ -12,15 +14,27 @@ class ProductListView(ListView):
 class ContactsView(TemplateView):
     template_name = "catalog/contacts.html"
 
-class ProductDetailView(DetailView):
+class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
+    login_url = reverse_lazy("users:login")
 
-class ProductCreateView(CreateView):
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        self.object = None
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.view_counter += 1
+        self.object.save()
+        return self.object
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     # fields = ('title', 'description', 'price', 'image', 'category')
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:product_list')
+    login_url = reverse_lazy("users:login")
 
     # def get_object(self, queryset=None):
     #     self.object = super().get_object(queryset)
@@ -34,12 +48,13 @@ class ProductDeleteView(DeleteView):
     template_name = "catalog/product_confirm_delete.html"
     success_url = reverse_lazy('catalog:product_list')
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     # fields = ("title", "description", "price", "image", "category")
     template_name = "catalog/product_form.html"
     success_url = reverse_lazy('catalog:product_list')
+    login_url = reverse_lazy("users:login")
 
     def get_success_url(self):
         return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
